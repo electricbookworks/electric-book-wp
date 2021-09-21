2e382db1151858960a32b634bfcbf2c65ed2efa6
Bertus Steenberg <bertus@electricbookworks.com>
2021-09-21 12:16:45 +0200

    initial code

--- wp-content/plugins/electric-book-wp/includes/user-profile.php
+++ wp-content/plugins/electric-book-wp/includes/user-profile.php
@@ -2,22 +2,34 @@
 
 defined('ABSPATH') || exit;
 
-function electric_book_wp_get_user_details($user_id)
-{
-    if (!$user = get_userdata($user_id)) {
-        return false;
-    }
-
-    return $user;
-}
-
+add_action('init', 'electric_book_wp_plugin_init');
 add_action('rest_api_init', function () {
     register_rest_route(
-        'core',
-        '/user/(?P<id>\d+)',
+        'core/v1',
+        '/user_details',
         [
             'methods' => 'GET',
             'callback' => 'electric_book_wp_get_user_details',
         ]
     );
 });
+
+
+function electric_book_wp_plugin_init(){
+    global $current_user, $electric_book_wp_user_id;
+    $electric_book_wp_user_id = $current_user->id;
+}   
+
+function electric_book_wp_get_user_details()
+{
+    global $electric_book_wp_user_id;
+
+    if (!$user = get_userdata($electric_book_wp_user_id)) {
+        return false;
+    }
+
+    $user_data = (array)$user->data;
+    unset($user_data['user_pass']);
+
+    return array_merge($user_data, ["capabilities" => $user->caps, "roles" => $user->roles]);
+}
